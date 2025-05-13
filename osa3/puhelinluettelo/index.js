0e5ea6d021f1cb9d3e003d80f9139fe69c7a1c9a
Laine Karolina <karolina.h.laine@student.jyu.fi>
2025-05-13 12:07:37 +0300

    osa5

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -5,6 +5,7 @@ const http = require('http')
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 let persons = [
     { 
       "name": "Arto Artonen", 
@@ -51,6 +52,23 @@ app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
   persons = persons.filter(person => person.id !== id)
   response.status(204).end() // 204 No Content
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'Name or number missing' })
+  }
+
+  const person = {
+    "name": body.name,
+    "number": body.number,
+    "id": (Math.round(Math.random() * 100000).toString())
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 
