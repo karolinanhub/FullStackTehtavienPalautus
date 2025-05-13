d62f28db15efa2d67203ced1029d7b470ed3209c
Laine Karolina <karolina.h.laine@student.jyu.fi>
2025-05-13 12:36:01 +0300

    osa8

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -4,10 +4,20 @@ const http = require('http')
 
 const express = require('express')
 const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms', 
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
 
 let persons = [
     { 
@@ -77,9 +87,6 @@ app.post('/api/persons', (request, response) => {
   persons = persons.concat(person)
   response.json(person)
 })
-
-
-
 
 
 const PORT = 3001
