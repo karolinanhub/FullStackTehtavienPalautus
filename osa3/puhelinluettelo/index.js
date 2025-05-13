0ddf98ea8bd4615941bbc21cef4f794ab85443ba
Laine Karolina <karolina.h.laine@student.jyu.fi>
2025-05-13 13:16:07 +0300

    osa 3.9

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -4,10 +4,13 @@ const http = require('http')
 
 const express = require('express')
 const morgan = require('morgan')
-
+const cors = require('cors')
 const app = express()
 
+app.use(cors()) // CORS (Cross-Origin Resource Sharing) on käytössä, jotta eri domainit voivat käyttää resursseja
+
 app.use(express.json())
+
 app.use(morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
