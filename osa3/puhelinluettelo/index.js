6c3181603abb5707fdf1ca5cba4d013aa2872787
Laine Karolina <karolina.h.laine@student.jyu.fi>
2025-05-13 13:45:37 +0300

    dist

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -7,9 +7,12 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
+
+app.use(express.static('dist'))
 app.use(cors()) // CORS (Cross-Origin Resource Sharing) on käytössä, jotta eri domainit voivat käyttää resursseja
 
 app.use(express.json())
+
 
 app.use(morgan(function (tokens, req, res) {
   return [
@@ -92,6 +95,6 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
