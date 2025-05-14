62ece4c94ab0c0962c0ee73492df445fb5859ee5
Laine Karolina <karolina.h.laine@student.jyu.fi>
2025-05-14 13:16:50 +0300

    3.14

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -89,13 +89,20 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+  /*const id = request.params.id
   const person = persons.find(person => person.id === id)
   if (person) {
     response.json(person)
   } else {
     response.status(404).end()
-  }
+  } */
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -111,18 +118,22 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({ error: 'Name or number missing' })
   }
 
-  if (persons.some(person => person.name === body.name)) {
-    return response.status(400).json({ error: 'Name must be unique' })
-  }
+ // if (persons.some(person => person.name === body.name)) {
+ //   return response.status(400).json({ error: 'Name must be unique' })
+ //  }
 
-  const person = {
+  const person = new Person({
     "name": body.name,
     "number": body.number,
-    "id": (Math.round(Math.random() * 100000).toString())
-  }
+    // "id": (Math.round(Math.random() * 100000).toString())
+  })
 
-  persons = persons.concat(person)
-  response.json(person)
+  //persons = persons.concat(person)
+  // response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+
 })
 
 
