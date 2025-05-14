b1ae3d6db44c57e9a06ff053061c4fa1f15195b0
Laine Karolina <karolina.h.laine@student.jyu.fi>
2025-05-14 14:09:17 +0300

    3.17

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -124,10 +124,8 @@ app.post('/api/persons', (request, response) => {
   if (!body.name || !body.number) {
     return response.status(400).json({ error: 'Name or number missing' })
   }
-
- // if (persons.some(person => person.name === body.name)) {
  //   return response.status(400).json({ error: 'Name must be unique' })
- //  }
+ 
 
   const person = new Person({
     "name": body.name,
