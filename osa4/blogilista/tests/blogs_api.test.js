849141c30a821a20c9f71215d18ace754238ad2d
Laine Karolina <karolina.h.laine@student.jyu.fi>
2025-05-20 10:35:34 +0300

    test_helper

--- osa4/blogilista/tests/blogs_api.test.js
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -5,35 +5,19 @@ const supertest = require('supertest')
 const app = require('../app')
 require('dotenv').config({ path: '.env' })
 const Blog = require('../models/blog')
+const helper = require('./test_helper')
 // superagent-olio
 //Supertest huolehtii testattavan sovelluksen käynnistämisestä sisäisesti käyttämäänsä porttiin. 
 const api = supertest(app)
 
-const initialBlogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  }
-]
+
 
 // Alustetaan tietokonata, ensin tyhjennetään ja sitten lisätään testidataa
 beforeEach(async () => {  
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
+  let blogObject = new Blog(helper.initialBlogs[0])
   await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
+  blogObject = new Blog(helper.initialBlogs[1])
   await blogObject.save()
 })
 
@@ -46,7 +30,7 @@ test('blogs are returned as json', async () => {
 
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, initialBlogs.length)
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
 test('identifier is "id"', async () => {
@@ -71,9 +55,10 @@ test('a valid blog can be added ', async () => {
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
-  const response = await api.get('/api/blogs')
-  const titles = response.body.map(r => r.title)
-  assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const titles = blogsAtEnd.map(r => r.title)
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
   assert(titles.includes('Type wars'))
 })
 
@@ -93,27 +78,16 @@ test('a blog without likes has 0 likes', async () => {
   const addedBlog = response.body.find(blog => blog.title === newBlog.title)
   assert.strictEqual(addedBlog.likes, 0)
 })
+
 test('a blog without title or url is not added', async () => {
-  const invalidBlogs = [
-    {
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/",
-      likes: 10,
-    },
-    {
-      author: "Robert C. Martin",
-      title: "First class tests",
-      likes: 10,
-    }
-  ]
-  for (const blog of invalidBlogs) { 
+  for (const blog of helper.invalidBlogs) { 
     await api
       .post('/api/blogs')
       .send(blog)
       .expect(400)
   }
-  const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, initialBlogs.length)
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
 })
 
 // lopuksi suljetaan yhteys tietokantaan
---
+++ osa4/blogilista/tests/test_helper.js
@@ -0,0 +1,44 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  }
+]
+
+const invalidBlogs = [
+    {
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/",
+    likes: 10,
+    },
+    {
+    author: "Robert C. Martin",
+    title: "First class tests",
+    likes: 10,
+    }
+    ]
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+    initialBlogs,
+    invalidBlogs,
+    blogsInDb,
+}
