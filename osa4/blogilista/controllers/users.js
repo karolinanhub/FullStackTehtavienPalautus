395c6ac5754a8342b330019bed221fa7aa7fd68f
Laine Karolina <karolina.h.laine@student.jyu.fi>
2025-05-22 13:34:18 +0300

    4.17

--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -3,8 +3,10 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 
+
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+    .find({}).populate('blogs' , { title: 1, author: 1, url: 1 }) //populate userin tiedot
   response.json(users)
 })
 
