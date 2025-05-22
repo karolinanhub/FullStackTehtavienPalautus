3359453956b0852c788c78942050db4250f8629c
Laine Karolina <karolina.h.laine@student.jyu.fi>
2025-05-22 13:49:39 +0300

    4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,6 +5,7 @@ const { MONGODB_URI, PORT } = require('./utils/config')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -21,9 +22,10 @@ mongoose
 
 // app.use(express.static('dist'))
 app.use(express.json())
-app.use(middleware.requestLogger)
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use(middleware.requestLogger)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
