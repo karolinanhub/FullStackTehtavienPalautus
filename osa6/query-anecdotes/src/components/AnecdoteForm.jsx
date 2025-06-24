1821583ccdeb44b06a35219b88ffb7329cdd9fb3
Laine Karolina <karolina.h.laine@student.jyu.fi>
2025-06-24 10:01:27 +0300

    6.24

--- osa6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ osa6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -24,6 +24,13 @@ const addAnecdote = async (event) => {
   event.preventDefault()
   const content = event.target.anecdote.value
   event.target.anecdote.value = ''
+  if (content.length < 5) {
+    dispatch({ type: 'SHOW', payload: 'Anecdote must be at least 5 characters long' })
+    setTimeout(() => {
+      dispatch({ type: 'HIDE' })
+    }, 5000)
+    return
+  }
   newAnecdotesMutation.mutate({ content })
   console.log('new anecdote', content)
   await dispatch({ type: 'SHOW', payload: `You added: ${content}` })
